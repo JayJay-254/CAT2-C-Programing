e3a695a996d26551317d3fa59bb3e763c8329826
JayJay-254 <jayjaydejason@gmail.com>
2024-11-06 10:52:28 -0500

    Add files via upload

---
+++ CAT2_Q3.C
@@ -0,0 +1,62 @@
+/*
+AUTHOR:JACKSON NJENGA
+REG NO:CT102/G/21788/23
+*/
+
+#include <stdio.h>
+/*
+
+float calculateGrossPay(float hoursWorked, float hourlyWage) {
+    float grossPay;
+    if (hoursWorked > 40) {
+        float overtimeHours = hoursWorked - 40;
+        grossPay = (40 * hourlyWage) + (overtimeHours * hourlyWage * 1.5);
+    } else {
+        grossPay = hoursWorked * hourlyWage;
+    }
+    return grossPay;
+}
+
+float calculateTax(float grossPay) {
+    float tax;
+    if (grossPay <= 600) {
+        tax = grossPay * 0.15;
+    } else {
+        tax = (600 * 0.15) + ((grossPay - 600) * 0.20);
+    }
+    return tax;
+}
+*/
+
+int main() {
+    float hoursWorked, hourlyWage, grossPay, tax, netPay;
+
+    // Input
+    printf("Enter hours worked in a week: ");
+    scanf("%f", &hoursWorked);
+    printf("Enter hourly wage: ");
+    scanf("%f", &hourlyWage);
+
+    if (hoursWorked > 40) {
+        float overtimeHours = hoursWorked - 40;
+        grossPay = (40 * hourlyWage) + (overtimeHours * hourlyWage * 1.5);
+    } else {
+        grossPay = hoursWorked * hourlyWage;
+    }
+    if (grossPay < 600) {
+        tax = grossPay * 0.15;
+    } else {
+        tax = (600 * 0.15) + ((grossPay - 600) * 0.20);
+    }
+
+    // Calculate netpay
+    
+    netPay = grossPay - tax;
+
+    // Output
+    printf("Gross Pay: $%.2f\n", grossPay);
+    printf("Tax: $%.2f\n", tax);
+    printf("Net Pay: $%.2f\n", netPay);
+
+    return 0;
+}
