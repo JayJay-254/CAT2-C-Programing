f8eee7758fd8cf99e84ce81443d9cb893524749c
JayJay-254 <jayjaydejason@gmail.com>
2024-11-06 19:08:00 +0300

    Update CAT2_Q3.C

--- CAT2_Q3.C
+++ CAT2_Q3.C
@@ -4,29 +4,7 @@ REG NO:CT102/G/21788/23
 */
 
 #include <stdio.h>
-/*
 
-float calculateGrossPay(float hoursWorked, float hourlyWage) {
-    float grossPay;
-    if (hoursWorked > 40) {
-        float overtimeHours = hoursWorked - 40;
-        grossPay = (40 * hourlyWage) + (overtimeHours * hourlyWage * 1.5);
-    } else {
-        grossPay = hoursWorked * hourlyWage;
-    }
-    return grossPay;
-}
-
-float calculateTax(float grossPay) {
-    float tax;
-    if (grossPay <= 600) {
-        tax = grossPay * 0.15;
-    } else {
-        tax = (600 * 0.15) + ((grossPay - 600) * 0.20);
-    }
-    return tax;
-}
-*/
 
 int main() {
     float hoursWorked, hourlyWage, grossPay, tax, netPay;
